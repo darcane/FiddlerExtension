6d2f80f8e84b4548eb35fed7edbbafeb56341dbd
Erdem Darcan <erdemdarcan@hotmail.com.tr>
2020-02-11 00:19:08 +0100

    Initial commit

---
+++ ComposerBodyFormatter/Darcane.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Fiddler;
+
+[assembly: RequiredVersion("2.3.5.0")]
+namespace ComposerBodyFormatter
+{
+    public class Darcane : IFiddlerExtension
+    {
+        private TabPage _oPage;
+        private DarcaneView _oView;
+        private EventHandler _tabActiveHandler;
+        private Session[] _sessions;
+
+        private MenuItem _oMenu;
+
+        public void OnLoad()
+        {
+            FiddlerApplication.UI.actToggleCapture();
+
+
+            _oPage = new TabPage("Darcane") { ImageIndex = 5 };
+            FiddlerApplication.UI.tabsViews.TabPages.Add(_oPage);
+            _tabActiveHandler = TabsViewsSelectedIndexChanged;
+            FiddlerApplication.UI.tabsViews.SelectedIndexChanged += _tabActiveHandler;
+
+            _oMenu = new MenuItem("Darcane") { Name = "Darcane Menu" };
+            FiddlerApplication.UI.mnuMain.MenuItems.Add(_oMenu);
+
+            DeleteUnwantedTabs();
+        }
+
+        private void TabsViewsSelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!FiddlerApplication.isClosing && FiddlerApplication.UI.tabsViews.SelectedTab == _oPage)
+            {
+                EnsureReady();
+            }
+        }
+
+        private void EnsureReady()
+        {
+            FiddlerApplication.UI.tabsViews.SelectedIndexChanged -= _tabActiveHandler;
+            _tabActiveHandler = null;
+            _oView = new DarcaneView();
+            _oPage.Controls.Add(_oView);
+            _oView.Dock = DockStyle.Fill;
+            _oView.AllowDrop = true;
+
+            FiddlerApplication.CalculateReport += FiddlerApplicationOnCalculateReport;
+
+            _sessions = FiddlerApplication.UI.GetSelectedSessions();
+
+            RecalcDisplay();
+        }
+
+        private void FiddlerApplicationOnCalculateReport(Session[] arrsessions)
+        {
+            _sessions = arrsessions;
+            RecalcDisplay();
+        }
+
+        private void RecalcDisplay()
+        {
+            // do your calculation here mate ;)
+        }
+
+        public void OnBeforeUnload()
+        {
+            _sessions = null;
+        }
+
+        private void DeleteUnwantedTabs()
+        {
+            var tabs = FiddlerApplication.UI.tabsViews.TabPages;
+            var unwantedTabs = new List<string> { "Get Started", "Fiddler Orchestra Beta" };
+            var indexes = new List<int>();
+            for (var i = 0; i < tabs.Count; i++)
+            {
+                if (unwantedTabs.Contains(tabs[i].Text))
+                {
+                    indexes.Add(i);
+                }
+            }
+            foreach (var i in indexes.OrderByDescending(x => x))
+            {
+                tabs.RemoveAt(i);
+            }
+        }
+    }
+}
---
+++ ComposerBodyFormatter/DarcaneView.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using Fiddler;
+
+namespace ComposerBodyFormatter
+{
+    public class DarcaneView: UserControl
+    {
+        private IContainer components;
+        private Panel _pnlControls;
+        public FlowLayoutPanel FlpItems;
+        public Button BtnTest;
+
+        public DarcaneView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            components = new Container();
+            _pnlControls = new Panel {Name = "pnlDarcane"};
+            FlpItems = new FlowLayoutPanel
+            {
+                AutoScroll = true,
+                BackColor = Color.White,
+                Cursor = Cursors.Default,
+                Dock = DockStyle.Fill,
+                Location = new Point(0, 25),
+                Name = "flpItems"
+            };
+
+            BtnTest = new Button {Name = "TestButton", Text = "Test Me!"};
+            BtnTest.Click += OnBtnTestOnClick;
+
+            Controls.Add(BtnTest);
+            Controls.Add(FlpItems);
+            Controls.Add(_pnlControls);
+
+            _pnlControls.ResumeLayout(false);
+            _pnlControls.PerformLayout();
+        }
+
+        private void OnBtnTestOnClick(object sender, EventArgs args)
+        {
+            var selectedSessions = FiddlerApplication.UI.GetSelectedSessions();
+            MessageBox.Show($"Hello Avensians !!\r\n{selectedSessions.Length} Session is selected! Good Job!");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                components?.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
